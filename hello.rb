68fbfd4656ac2db50b0554dae65fc2eaea8d6917
Mayank <mswebsite01@gmail.com>
2023-01-31 18:50:23 +0530

    Initial commit

---
+++ hello.rb
@@ -0,0 +1,25 @@
+# firstName = 'Mayank'
+# lastName = 'Singh'
+
+# p "My name is #{firstName} #{lastName}"
+
+# city = "Delhi"
+# newCity = city
+
+# city = "Dehradun"
+
+# p newCity
+
+# p 10 === 10.0
+# p 10.eql?(10.0)
+
+# ranges 
+# x = 1..100
+# p x.to_a.shuffle
+
+# arrays 
+# arr = [1, 2, "Hello"]
+# p arr.class
+
+# arr2 = [1..10]
+# p arr2[0].to_a
